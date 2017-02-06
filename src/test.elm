b81b3ef500918e05c3eabbc7de7aab366707a82a
Anders Eide <Anders.kaare.eide@gmail.com>
2017-02-06 17:19:39 +0100

    Messed around with the json package for Elm, trying to remember syntax and the like

--- src/test.elm
+++ src/test.elm
@@ -1,9 +1,11 @@
 module Main exposing (..)
 
 import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (style, class)
+import Json.Decode exposing (..)
 
 
+main : Program Never Model Msg
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
 
@@ -19,6 +21,26 @@ type alias Model =
 model : Model
 model =
     0
+
+
+json : String
+json =
+    """[
+    {
+    "Ref.No": "AT-2017-000405",
+    "Country": "Austria",
+    "Year": 2017,
+    "Deadline": "2017-03-31",
+    "Expire": "2017-03-31"
+  },
+  {
+    "Ref.No": "BE-2017-000001",
+    "Country": "Belgium",
+    "Year": 2017,
+    "Deadline": "2017-03-31",
+    "Expire": "2017-03-31"
+  }
+  ]"""
 
 
 
@@ -50,9 +72,24 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        , button [ onClick Reset ] [ text "Reset" ]
-        ]
+    div [] (buildDivs (decode json))
+
+
+buildDivs : List String -> List (Html Msg)
+buildDivs list =
+    case list of
+        head :: tail ->
+            buildDiv head :: buildDivs tail
+
+        _ ->
+            []
+
+
+buildDiv : String -> Html Msg
+buildDiv input =
+    div [ style [ ( "background", "pink" ) ], class "country" ] [ text input ]
+
+
+decode : String -> List String
+decode input =
+    Result.withDefault [] (decodeString (list (field "Country" string)) input)
