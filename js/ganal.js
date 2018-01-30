4ead4f9b0733adbb603a4bd57c8bea5c973fe923
eddiman <edbjorgen@gmail.com>
2018-01-30 15:35:28 +0100

    made it ready for live usage update content security policy and other fixes in paths

---
+++ js/ganal.js
@@ -0,0 +1,8 @@
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-3101590-4"></script>
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-3101590-4');
